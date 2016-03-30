d69d8855f4e465a3093665ef1b5491c9e0754ac4
Diego Serrano <Diego Serrano>
2016-03-30 11:48:46 -0300

    Correcciones de la clase 3

--- 1-3-Personas/src/personas/Main.java
+++ 1-3-Personas/src/personas/Main.java
@@ -3,15 +3,23 @@ package personas;
 public class Main {
 
     public static void main(String[] args) {
-
         Persona per1, per2;
         
-        per1 = new Persona("Juan","Perez",30);
-        per2 = new Persona("Juana","Pereza",20);
+        per1 = new Persona("Juan","Perez",1989);
+        per2 = new Persona("Juana","Pereza",1982);
         
-        System.out.println(per1);
+        System.out.println(per1.getNombre());
+        System.out.println(per1.getApellido());
+        System.out.println(per1.getNacimiento());
+        System.out.println(per1.nombreCompleto());
+        System.out.println(per1.edad());
         
-        System.out.println(per2);
+        System.out.println(per2.getNombre());
+        System.out.println(per2.getApellido());
+        System.out.println(per2.getNacimiento());
+        System.out.println(per2.nombreCompleto());
+        System.out.println(per2.edad());
+        
     }
     
 }
--- 1-3-Personas/src/personas/Persona.java
+++ 1-3-Personas/src/personas/Persona.java
@@ -1,12 +1,9 @@
 package personas;
 
 public class Persona {
-
     private String nombre;
     private String apellido;
-    private int edad;
-    private int altura;
-    private int peso;
+    private int nacimiento;
 
     public String getNombre() {
         return nombre;
@@ -24,52 +21,34 @@ public class Persona {
         this.apellido = apellido;
     }
 
-    public int getEdad() {
-        return edad;
+    public int getNacimiento() {
+        return nacimiento;
     }
 
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public int getAltura() {
-        return altura;
-    }
-
-    public void setAltura(int altura) {
-        this.altura = altura;
-    }
-
-    public int getPeso() {
-        return peso;
-    }
-
-    public void setPeso(int peso) {
-        this.peso = peso;
+    public void setNacimiento(int nacimiento) {
+        this.nacimiento = nacimiento;
     }
 
     public Persona() {
     }
 
-    public Persona(String nombre, String apellido, int edad, int altura, int peso) {
+    public Persona(String nombre, String apellido, int nacimiento) {
         this.nombre = nombre;
         this.apellido = apellido;
-        this.edad = edad;
-        this.altura = altura;
-        this.peso = peso;
+        this.nacimiento = nacimiento;
     }
 
-    public String nombreCompleto() {
-        return nombre + " " + apellido;                
-    }
-
-    public float imc() {
-        return peso / (altura * altura);
-    }
-    
     @Override
     public String toString() {
-        return "Persona{" + "apellido=" + apellido + ", edad=" + edad + ", altura=" + altura + ", peso=" + peso + '}';
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", nacimiento=" + nacimiento + '}';
+    }
+    
+    public String nombreCompleto() {
+        return nombre + " " + apellido;
+    }
+    
+    public int edad() {
+        return 2016-nacimiento;
     }
     
     

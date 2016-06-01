24d947667dd84666488f7700323e59138ffc4328
diegojserrano <diegojserrano@gmail.com>
2016-06-01 10:07:13 -0300

    BoxLayout

--- 5-1-JFrame/src/pkg5/pkg1/jframe/JFrame1.java
+++ 5-1-JFrame/src/pkg5/pkg1/jframe/JFrame1.java
@@ -11,22 +11,22 @@ public class JFrame1 {
         JFrame ventana = new JFrame("Título de la ventana");
         ventana.setSize(300, 200);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.setLayout(new BorderLayout());
+        ventana.setLayout(new BoxLayout(ventana.getContentPane(), BoxLayout.X_AXIS));
         
-        JButton boton1 = new JButton("Aceptar");
-        ventana.add(boton1, BorderLayout.NORTH);
+        JButton boton1 = new JButton("Boton 1");
+        ventana.add(boton1);
         
-        JButton boton2 = new JButton("Cancelar");
-        ventana.add(boton2, BorderLayout.CENTER);
+        JButton boton2 = new JButton("Boton 2");
+        ventana.add(boton2);
         
         JButton boton3 = new JButton("Boton 3");
-        ventana.add(boton3, BorderLayout.SOUTH);
+        ventana.add(boton3);
         
         JButton boton4 = new JButton("Boton 4");
-        ventana.add(boton4, BorderLayout.WEST);
+        ventana.add(boton4);
         
         JButton boton5 = new JButton("Boton 5");
-        ventana.add(boton5, BorderLayout.EAST);
+        ventana.add(boton5);
         
 
         ventana.setVisible(true);

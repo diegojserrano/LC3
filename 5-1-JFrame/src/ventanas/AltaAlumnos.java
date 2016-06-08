d055a21fd3f46eafcb179b2d917d38f9232e377a
diegojserrano <diegojserrano@gmail.com>
2016-06-08 17:55:48 -0300

    Ejercicios resueltos en la clase 12 (1/6/16)

---
+++ 5-1-JFrame/src/ventanas/AltaAlumnos.java
@@ -0,0 +1,110 @@
+package ventanas;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class AltaAlumnos extends javax.swing.JFrame {
+
+    private ArrayList<Alumno> listaAlumnos;
+
+    public AltaAlumnos() {
+        initComponents();
+        listaAlumnos = new ArrayList<>();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnAgregar = new javax.swing.JButton();
+        txtLegajo = new javax.swing.JTextField();
+        txtNombre = new javax.swing.JTextField();
+        txtPromedio = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Agregar alumno");
+        setResizable(false);
+
+        btnAgregar.setText("Agregar");
+        btnAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Legajo");
+
+        jLabel2.setText("Nombre");
+
+        jLabel3.setText("Promedio");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnAgregar)
+                    .addComponent(txtPromedio)
+                    .addComponent(txtNombre)
+                    .addComponent(txtLegajo, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(50, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtLegajo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtPromedio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addComponent(btnAgregar)
+                .addContainerGap(28, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarActionPerformed
+        int legajo = Integer.parseInt(txtLegajo.getText());
+        String nombre = txtNombre.getText();
+        float promedio = Float.parseFloat(txtPromedio.getText());
+
+        Alumno nuevo = new Alumno(nombre, legajo, promedio);
+        listaAlumnos.add(nuevo);
+        String mensaje = "Cantidad de alumnos: " + listaAlumnos.size() + "\n" + listaAlumnos.toString();
+                
+        JOptionPane.showMessageDialog(null,mensaje);
+        
+        txtNombre.setText("");
+        txtLegajo.setText("");
+        txtPromedio.setText("");
+    }//GEN-LAST:event_btnAgregarActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAgregar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField txtLegajo;
+    private javax.swing.JTextField txtNombre;
+    private javax.swing.JTextField txtPromedio;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ 5-1-JFrame/src/ventanas/Principal.java
@@ -0,0 +1,10 @@
+package ventanas;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        AltaAlumnos v = new AltaAlumnos();
+        v.setVisible(true);
+    }
+}

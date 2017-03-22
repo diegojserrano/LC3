ccd074038dca3ff16730d5c8889cb01acd4966b7
diegojserrano <diegojserrano@gmail.com>
2017-03-22 15:16:46 -0300

    Corrección menor

--- 1-1-TresArticulos/src/TresArticulos.java
+++ 1-1-TresArticulos/src/TresArticulos.java
@@ -9,19 +9,19 @@ public class TresArticulos {
         float precio1, precio2, precio3;
         int cantidad1, cantidad2, cantidad3;
         
-        System.out.print("Ingrese precio del primer artículo");
+        System.out.print("Ingrese precio del primer artículo ");
         precio1 = sc.nextFloat();
-        System.out.print("Ingrese cantidad del primer artículo");
+        System.out.print("Ingrese cantidad del primer artículo ");
         cantidad1 = sc.nextInt();
         
-        System.out.print("Ingrese precio del segundo artículo");
+        System.out.print("Ingrese precio del segundo artículo ");
         precio2 = sc.nextFloat();
-        System.out.print("Ingrese cantidad del segundo artículo");
+        System.out.print("Ingrese cantidad del segundo artículo ");
         cantidad2 = sc.nextInt();
         
-        System.out.print("Ingrese precio del tercer artículo");
+        System.out.print("Ingrese precio del tercer artículo ");
         precio3 = sc.nextFloat();
-        System.out.print("Ingrese cantidad del tercer artículo");
+        System.out.print("Ingrese cantidad del tercer artículo ");
         cantidad3 = sc.nextInt();
         
         float subtotal1 = precio1 * cantidad1;
@@ -29,7 +29,7 @@ public class TresArticulos {
         float subtotal3 = precio3 * cantidad3;
         float total = subtotal1 + subtotal2 + subtotal3;
         
-        System.out.println("El total de la factura es de $" + total);
+        System.out.println("El total de la factura es de $ " + total);
     }
     
 }
